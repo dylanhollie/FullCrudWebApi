6e586104938eedd4fbb448566ebcfd71c289eb97
dylanhollie <dylanhollie03@gmail.com>
2025-02-03 22:39:00 +0000

    Sql Package Addition

---
+++ FullCrudWebAPI-Dot8/FullCrudWebAPI-Dot8/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using FullCrudWebAPI_Dot8.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullCrudWebAPI_Dot8.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
